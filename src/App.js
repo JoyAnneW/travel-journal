a411e684acae1bc62645bd24f64b92364f134ad1
JoyAnneW <joyawilliams@gmail.com>
2022-01-17 16:01:00 +0100

    completed styling on card component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			{cardComponent}
+			<main>{cardComponent}</main>
 		</div>
 	);
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,17 @@ import React from "react";
 
 export default function Card(props) {
 	return (
-		<main className="card">
+		<div className="card">
 			<img src={props.imagePath} alt={props.title} className="card--img" />
 			<div className="card__details">
 				<i class="fas fa-map-marker-alt"></i>
 				<span className="card__details--country">{props.location}</span>
-				<a href={props.googleMapsUrl} className="card__details--map">
+				<a
+					href={props.googleMapsUrl}
+					target="_blank"
+					rel="noreferrer"
+					className="card__details--map"
+				>
 					View on Google Maps
 				</a>
 				<h1 className="card__details--title">{props.title}</h1>
@@ -17,6 +22,6 @@ export default function Card(props) {
 
 				<p className="card__details--text">{props.description}</p>
 			</div>
-		</main>
+		</div>
 	);
 }
