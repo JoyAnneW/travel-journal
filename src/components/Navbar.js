0a8785ad205be83325a85a30693f8d56f7e0fd0c
JoyAnneW <joyawilliams@gmail.com>
2022-01-17 10:51:58 +0100

    created and styled navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,10 @@
 import React from "react";
+
+export default function Navbar() {
+	return (
+		<nav className="navbar">
+			<i class="fas fa-globe-americas"></i>
+			<h1 className="navbar--heading">My Travel Journal</h1>
+		</nav>
+	);
+}
